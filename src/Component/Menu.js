63a9dbd7e7e9c63fef59a8b17e6d9108e0da6fc5
priyanshu <priyanshu.cs21@sahyadri.edu.in>
2023-07-14 16:09:49 +0530

    done

---
+++ src/Component/Menu.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Menu = () => {
+  return (
+    <div>
+      <header>
+        <h1>Menu</h1>
+      </header>
+      <section>
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <h2>Food Items</h2>
+              {/* Add your food item components here */}
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Menu;
